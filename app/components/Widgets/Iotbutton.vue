26b993511d19b5a4518454e8c850affa44641ac2
skyark21 <skyark21@github.com>
2022-01-22 08:55:00 -0700

    Working in deplying custom button-widget

--- app/components/Widgets/Iotbutton.vue
+++ app/components/Widgets/Iotbutton.vue
@@ -3,29 +3,47 @@
         <div slot='header'>
             <h4 class='card-title'>{{config.selectedDevice.name}} - {{config.variableFullName}}</h4>
         </div>
-        <i class='fa ' :class="[config.icon, getIconColorClass()]" style="font-size: 30px"></i>
+        <i
+            class='fa ' 
+            :class="[config.icon, getIconColorClass()]" 
+            style="font-size: 30px"
+        ></i>
+        <base-button 
+            @click="sendValue()" 
+            :type="config.class" 
+            class="mb-3 pull-right" 
+            size="lg"
+        >SEND</base-button>
     </card>
 </template>
-
 <script>
     export default {
         props: ['config'],
         data() {
             return{
-                value: true,
-            };
+                sending: false,
+            }
         },
         mounted(){
             this.$nuxt.$on('widget-topic', this.processReceivedData)
         },
         methods:{
-            processReceivedData(data){
-                console.log('received');
-                console.log(data);
-                this.value = data.value;
+            sendValue(){
+                this.sending = true;
+                setTimeout(() =>{
+                    this.sending = false;
+                }, 1000);
+                const TOSEND ={
+                    topic: this.config.userId + "/" + this.config.selectedDevice.dId + "/" + this.config.variable + "/acdata",
+                    msg: {
+                        value : this.config.message
+                    }
+                };
+                console.log(TOSEND);
+                this.$nuxt.$emit('mqtt-sender', TOSEND);
             },
             getIconColorClass(){
-                if(!this.value){
+                if(!this.sending){
                     return "text-dark";
                 }
                 if(this.config.class == "success"){
@@ -40,7 +58,7 @@
                 if(this.config.class == "danger"){
                     return "text-danger";
                 }
-            }
+            },
         }
     }
 </script>
--- app/pages/templates.vue
+++ app/pages/templates.vue
@@ -1,30 +1,60 @@
 <template>
     <div>
-        <Iotindicator></Iotindicator>
-        <base-button type="primary" class="mb-2" size="lg" @click="sendData()">SEND</base-button>
+        <Iotindicator :config=configIndicator></Iotindicator>
+        <Iotbutton :config="configButton"></Iotbutton>
     </div>
 </template> 
 
 <script>
+    import Iotbutton from '../components/Widgets/Iotbutton.vue';
     import Iotindicator from '../components/Widgets/Iotindicator.vue'
 
     export default {
         data(){
             return{
-                value:false
+            value:false,
+                configButton:{
+                    userId: 'userid',
+                    selectedDevice:{
+                        name: 'Home',
+                        dId: '45983',
+                        templateName: 'Power Sensor',
+                        tempalateId: '155651653515151',
+                        saverRule: false,
+                    },
+                    variableFullName: 'Boiler',
+                    variable:'uniquestr',
+                    icon: "fa-sun",
+                    column: 'col-6',
+                    widget: 'indicator',
+                    class: 'danger',
+                    message: '{"fanstatus": "stop"}'
+                },
+                configIndicator:{
+                    userId: 'userid',
+                    selectedDevice:{
+                        name: 'Home',
+                        dId: '56812',
+                        templateName: 'Power Sensor',
+                        tempalateId: '155651653515151',
+                        saverRule: false,
+                    },
+                    variableFullName: 'Boiler',
+                    variable:'uniquestr',
+                    icon: "fa-sun",
+                    column: 'col-6',
+                    widget: 'indicator',
+                    class: 'danger',
+                    message: '{"fanstatus": "stop"}'
+                },
             }
         },
         methods:{
-            sendData(){
-                this.value = !this.value;
-                const toSend = {
-                    value: this.value,
-                }
-                this.$nuxt.$emit('widget-topic', toSend);
-            }
+
         },
         components: {
             Iotindicator,
+            Iotbutton,
         },
     }
 </script>
